33f11a8c4ef9060efa86fdda921f2d7b437bb769
Timothy Chong <timothychong@outlook.com>
2015-04-26 16:35:43 -0400

    Added matrix files HUGE

--- main.cu
+++ main.cu
@@ -4,46 +4,91 @@
 #include "main.h"
 #include <cstdio>
 
-
+#define TOL 1e-4
 
 int main() {
+	const char filename[] = "matrix/2000.txt";
 
-	const char filename[] = "matrix.txt";
-
+	//READING FROM FILE
 	data_t * input;
 	int len;
-
 	read_matrix_from_file(&input, filename, & len);
-	print_matrix(input, len);
+	//END READING FROM FILE
 
-	data_t input2[len * len];
-	copy_matrix(input, input2, len);
-	print_matrix(input, len);
-	chsky_dec_baseline(input, len);
-	printf("\n");
-	print_matrix(input, len);
-
-	//Allocating memory on 
-	size_t allocSize = len * len * sizeof(data_t);
+	printf("Matrix dimension: %d X %d\n", len, len);
 	
 	// Allocate GPU memory
+	size_t allocSize = len * len * sizeof(data_t);
 	data_t * d_matrix;
 	CUDA_SAFE_CALL(cudaMalloc((void **)&d_matrix, allocSize));
 
 	// Allocate arrays on host memory
 	data_t *h_result = (float *) malloc(allocSize);
 
-	CUDA_SAFE_CALL(cudaMemcpy(d_matrix, input2, allocSize, cudaMemcpyHostToDevice));
+	// GPU Timing variables
+	cudaEvent_t start, stop;
+	float elapsed_gpu;
+
+	// Create the cuda events
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	// Record event on the default stream
+	cudaEventRecord(start, 0);
+
+	CUDA_SAFE_CALL(cudaMemcpy(d_matrix, input, allocSize, cudaMemcpyHostToDevice));
 
 	chsky_dec_gpu(d_matrix, len);
 
 	CUDA_SAFE_CALL(cudaMemcpy(h_result, d_matrix, allocSize, cudaMemcpyDeviceToHost));
 
-	print_matrix(h_result, len);
+	// Stop and destroy the timer
+	cudaEventRecord(stop,0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsed_gpu, start, stop);
+
+	printf("\nGPU time: %f (msec)\n", elapsed_gpu);
+
+	cudaEventRecord(start, 0);
+
+	chsky_dec_baseline(input, len);
+
+	cudaEventRecord(stop,0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsed_gpu, start, stop);
+	printf("\nCPU time: %f (msec)\n", elapsed_gpu);
+
+
+	int errCount = 0, zeroCount = 0;
+	float max_diff = 0;
+
+	for(int i = 0; i < len * len; i++) {
+		double diff = abs(h_result[i] - input[i]);
+		if (diff > TOL) {
+			if(diff > max_diff)
+					max_diff = diff;
+			errCount++;
+		}
+		if (h_result[i] == 0) {
+			zeroCount++;
+		}
+	}
+	printf("Maximum difference: %f", max_diff);
+	
+	if (errCount > 0) {
+		printf("\n@ERROR: TEST FAILED: %d results did not matched\n", errCount);
+	}
+	else if (zeroCount > 0){
+		printf("\n@ERROR: TEST FAILED: %d results (from GPU) are zero\n", zeroCount);
+	}
+	else {
+		printf("\nTEST PASSED: All results matched\n");
+	}
 
 	CUDA_SAFE_CALL(cudaFree(d_matrix));
 	free(h_result);
 	free(input);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 }
 
 
